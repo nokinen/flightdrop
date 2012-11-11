b585759afe1a899b3dc0e4eb0cf8c77277dce006
Tobias Noiges <tobias@noig.es>
2012-11-11 20:57:46 +0100

    Add alert for unsupported browsers

--- public/js/drop.js
+++ public/js/drop.js
@@ -1,4 +1,12 @@
 $(function(){
+	
+	// Read files with FileReader
+	if(!window.FileReader){
+		$("#main .page-header").after('<div class="alert alert-block"><h4>Unsupported Browser</h4>Use a modern browser such as Chrome, Safari or Firefox to access this page.</div>');
+		console.error("FileReader API not supported by this browser");
+		return;
+	}
+	
 	var dropzone = $("#dropzone");
 	dropzone.on("dragover",allowDrop);
 	dropzone.on("drop",drop);
@@ -9,12 +17,6 @@ function drop(event) {
 	
 	var file = event.originalEvent.dataTransfer.files[0];
 	console.log("Dropped file:" + file.name);
-		
-	// Read files with FileReader
-	if(!window.FileReader){
-		console.error("FileReader API not supported by this browser");
-		return;
-	}
 		
 	var reader = new FileReader();
 	reader.onloadend = function(e){
